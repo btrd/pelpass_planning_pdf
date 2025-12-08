a6468732e38511143282ecd771916f7ae5bb57d4
Jean Bertrand <github@jean.btrd.me>
2025-12-08 14:33:54 +0100

    Change url

--- server.rb
+++ server.rb
@@ -2,7 +2,7 @@ require 'debug'
 require 'sinatra'
 require_relative 'lib/generator'
 
-get '/generate_zip' do
+get '/pelpass' do
     filename = Planning::Generator.new(params[:url]).run
     send_file filename, filename: filename, type: 'application/zip', disposition: 'attachment'
 end
